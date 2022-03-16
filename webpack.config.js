5c143d3b19d7348138cf871931571f64887bb8e9
MGonza20 <64711979+MGonza20@users.noreply.github.com>
2022-03-15 20:50:58 -0600

    quitando comentarios para bundle con babel

--- webpack.config.js
+++ webpack.config.js
@@ -126,17 +126,17 @@ module.exports = {
             }
           }
         },
-        //{
-          //test: /\.m?js$/,
-          //exclude: /(node_modules)/,
-          //use: {
-            //loader: 'babel-loader',
-            //options: {
-              //presets: ['@babel/preset-env'],
-              //plugins: ['babel-plugin-hash-resolve'],
-            //}
-          //}
-        //}
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['babel-plugin-hash-resolve'],
+            }
+          }
+        }
     ],
   },
 };
